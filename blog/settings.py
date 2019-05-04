5bb015fdfc2c2994776bd770e3c56aa1acfd3cfd
EarthmanAman <hashimathman@gmail.com>
2019-05-04 23:00:02 +0300

    sitemaps

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
+    'django.contrib.sites',
     'accounts',
     'comment',
     'ckeditor',
